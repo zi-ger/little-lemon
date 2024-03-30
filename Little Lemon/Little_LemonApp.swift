ffcf7feacba9b870d16ce1bc703e7ae7258673f1
Gustavo Ziger <gustavoziger@gmail.com>
2024-03-30 01:08:25 -0300

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Gustavo Ziger on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
