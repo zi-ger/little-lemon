189855185bf11ce0efa055b3ada16a14ef2b0b06
Gustavo Ziger <gustavoziger@gmail.com>
2024-03-30 18:23:55 -0300

    Updates UserProfile layout

--- Little Lemon/Views/UserProfile.swift
+++ Little Lemon/Views/UserProfile.swift
@@ -15,22 +15,30 @@ struct UserProfile: View {
     @Environment(\.presentationMode) var presentation
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Personal Information")
+                .font(.system(size: 24))
+                .bold()
             Image("profile-image-placeholder").resizable()
                 .aspectRatio(contentMode: .fit)
             
-            TextField("First Name", text: $firstName)
-            TextField("Last Name", text: $lastName)
-            TextField("Email", text: $email)
-            
-            Button("Save changes") {
-                UserDefaults.standard.setValue(firstName, forKey: kFirstName)
-                UserDefaults.standard.setValue(lastName, forKey: kLastName)
-                UserDefaults.standard.setValue(email, forKey: kEmail)
+            VStack(alignment: .leading, spacing: 0) {
+                Text("First Name")
+                    .font(.system(size: 12))
+                TextField("First Name", text: $firstName)
+            }
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Last Name")
+                    .font(.system(size: 12))
+                TextField("Last Name", text: $lastName)
+            }
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Email")
+                    .font(.system(size: 12))
+                TextField("Email", text: $email)
             }
             
-            Button("Logout") {
+            Button("Log out") {
                 UserDefaults.standard.removeObject(forKey: kFirstName)
                 UserDefaults.standard.removeObject(forKey: kLastName)
                 UserDefaults.standard.removeObject(forKey: kEmail)
@@ -39,8 +47,40 @@ struct UserProfile: View {
                 
                 self.presentation.wrappedValue.dismiss()
             }
+            .frame(maxWidth: .infinity)
+            .tint(.black)
+            .padding()
+            .cornerRadius(25)
+            .background(Color(red: 242 / 255, green: 199 / 255, blue: 52 / 255))
+            
+            HStack {
+                Spacer()
+                NavigationLink(destination: Menu()) {
+                    Button("Discard changes") {
+                        
+                    }
+                    .frame(width: 150, height: 40)
+                    .cornerRadius(15)
+                    .tint(.black)
+                    .border(Color(red: 64 / 255, green: 83 / 255, blue: 77 / 255))
+                }
+                Button("Save changes") {
+                    UserDefaults.standard.setValue(firstName, forKey: kFirstName)
+                    UserDefaults.standard.setValue(lastName, forKey: kLastName)
+                    UserDefaults.standard.setValue(email, forKey: kEmail)
+                }
+                .frame(width: 150, height: 40)
+                .cornerRadius(15)
+                .tint(.white)
+                .background(Color(red: 64 / 255, green: 83 / 255, blue: 77 / 255))
+                Spacer()
+            }
+            
+            
             Spacer()
         }
+        .padding()
+        .textFieldStyle(.roundedBorder)
     }
 }
 
